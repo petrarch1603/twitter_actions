7f9c23bf748d8022293fd786d6b7bc754ee8e951
patrickmcgranaghan <petrarch1603@gmail.com>
2019-07-30 05:28:44 -0600

    check for new hashtag requests

--- exec_twitter_post.py
+++ exec_twitter_post.py
@@ -1,4 +1,9 @@
 import Database
+import twitter_activities
+
+my_ht = '#mappornrequest'
+
+twitter_activities.process_requests(hashtag=my_ht)
 
 act_db = Database.ActionDB(testing=0).execute_actions()
 
