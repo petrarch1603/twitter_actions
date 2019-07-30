60e4824a213d38c872be9bb77a27d95c260c6da1
patrickmcgranaghan <petrarch1603@gmail.com>
2019-07-29 20:17:48 -0600

    initial commit

---
+++ exec_prc_hashtag.py
@@ -0,0 +1,5 @@
+import twitter_activities
+
+my_ht = '#mappornrequest'
+
+twitter_activities.process_requests(hashtag=my_ht)
---
+++ exec_prc_requests.py
@@ -0,0 +1,3 @@
+import twitter_activities
+
+twitter_activities.respond_to_requests()
