de5939622570988b70336d508d64a35035bb4227
patrickmcgranaghan <petrarch1603@gmail.com>
2019-08-10 11:44:04 -0600

    print remaining unexecuted actions at the end of script

--- exec_csv_to_db.py
+++ exec_csv_to_db.py
@@ -4,4 +4,4 @@ import my_helper
 
 my_helper.add_csv_to_database(csv_path='data_discover_followers/processing_queue.csv')
 
-act_db = Database.ActionDB()
+print(f"Remaining Unexecuted Actions = {len(Database.ActionDB().complete_unexecuted_list())}")
